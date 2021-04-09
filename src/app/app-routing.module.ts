f2c37b47dada99811bcbe0f82c3a6b8cab35c4d0
William Silva <williaamsf@gmail.com>
2021-04-08 23:36:58 -0300

    Adiciona base do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: '', pathMatch: 'full', redirectTo: 'home'
+      },
+      {
+        path: '',
+        loadChildren: () => import('@modules/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      },
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
